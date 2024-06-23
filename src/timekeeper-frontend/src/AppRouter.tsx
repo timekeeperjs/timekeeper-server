f29374c8d882bbdf5829f6e49b407ba0c48e430c
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-06-23 15:41:35 +0700

    [ADD] timekeeper-frontend

---
+++ src/timekeeper-frontend/src/AppRouter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
